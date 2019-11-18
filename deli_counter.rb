eaea74e95c4c7deff31a223ccb0056bfb40820fa
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2019-11-18 23:28:09 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,12 +1,12 @@
 # Write your code here.
 #line shows everyone's spot in line
-def line(people_in_line)
-  if (people.size == 0)
+def line(people_in_line = [])
+  if (people_in_line.size == 0)
     puts "The line is currently empty."
     return
   end
   line_message = "The line is currently:"
-  people.each_index do |current_spot|
+  people_in_line.each_index do |current_spot|
     line_message << " #{current_spot + 1}. #{people[current_spot]}"
   end
   puts line_message
