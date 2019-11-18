f8b6a6fa3c015e218bdeb01dbd3a998ebeda5a04
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2019-11-18 22:59:14 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,8 +1,12 @@
 # Write your code here.
 #line shows everyone's spot in line
 def line(people)
+  if (people.size == 0)
+    return "line is empty"
+  end
   people.each_index do |current_spot|
     puts "#{people[current_spot]}'s position in line is #{current_spot + 1}."
+  end
 end
 #take_a_number
 
