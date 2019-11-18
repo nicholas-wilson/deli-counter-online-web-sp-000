98c50a30dfb475a42176ca2fb814e35a70082922
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2019-11-18 22:57:49 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,8 @@
 # Write your code here.
 #line shows everyone's spot in line
 def line(people)
+  people.each_index do |current_spot|
+    puts "#{people[current_spot]}'s position in line is #{current_spot + 1}."
 end
 #take_a_number
 
